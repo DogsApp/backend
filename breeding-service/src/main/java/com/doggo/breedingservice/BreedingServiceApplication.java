98176db7043f7ddbc64e7de886989127bee37e1e
Matt <mattwasilewski96@gmail.com>
2023-05-14 16:41:26 +0200

    microservices initialization

---
+++ breeding-service/src/main/java/com/doggo/breedingservice/BreedingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.doggo.breedingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreedingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreedingServiceApplication.class, args);
+	}
+
+}
---
+++ walking-service/src/test/java/com/doggo/walkingservice/WalkingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doggo.walkingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalkingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
