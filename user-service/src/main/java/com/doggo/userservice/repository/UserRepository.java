f5775f0d12a1981df5bfd0660471b65a0ec77ed0
Matt <mattwasilewski96@gmail.com>
2023-06-26 22:55:41 +0200

    added responses and requests classes for auth functionality, working version with register and login

--- user-service/src/main/java/com/doggo/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/doggo/userservice/repository/UserRepository.java
@@ -1,13 +1,16 @@
 package com.doggo.userservice.repository;
 
-import com.doggo.userservice.entity.UserEntity;
+import java.util.Optional;
+
+import com.doggo.userservice.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-import java.util.UUID;
-
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, UUID> {
-    Optional<UserEntity> findByEmail(String email);
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
 }
